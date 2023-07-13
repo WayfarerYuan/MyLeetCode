bb17cee349a81d011938e0be0a99f037c93d101d
Wayfarer Yuan <voyageryuan@outlook.com>
2023-07-13 22:46:53 +0100

    0713

---
+++ 169 多数元素/Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {3, 2, 3};
+        System.out.println(solution.majorityElement(nums));
+    }
+}
---
+++ 189 旋转数组/RotateArray/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        solution.rotate(nums, 3);
+    }
+}
